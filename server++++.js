7ee5afb94a3ce14e1b8a48da58d5b8c9451180d5
staffuser <119116574+staffuser@users.noreply.github.com>
2023-09-02 18:35:49 +0300

    Update server++++.js

--- server++++.js
+++ server++++.js
@@ -1,1 +1,11 @@
+app.get('/search', (req, res) => {
+  const searchQuery = req.query.q;
+  if (!searchQuery) {
+    res.redirect(302, '/');
+    return;
+  }
+
+  console.log(searchQuery);
+  res.status(200).end();
+});
 
